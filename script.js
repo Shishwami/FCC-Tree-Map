22955ef60f29348e78d6ddb27b05433d3038ba9c
Shishwami <157940784+Shishwami@users.noreply.github.com>
2025-01-05 10:15:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,32 @@ const datasets = {
     movie: "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json"
 };
 
+const datasetKey = getQueryParam("data") || "video";
+loadDataset(datasetKey);
+
+const width = 1000;
+const height = 600;
+const padding = 60;
+
+const svg = d3.select("#treemap")
+    .attr("width", width)
+    .attr("height", height);
+
+const tooltip = d3.select("#tooltip");
+
+
 function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
     console.log(param);
     return urlParams.get(param);
 }
 
-const datasetKey = getQueryParam("data") || "video";
+function loadDataset(datasetKey) {
+    const dataUrl = datasets[datasetKey];
+    fetch(dataUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
 
-console.log(datasetKey);
+        });
+}
