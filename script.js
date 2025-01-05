5833c1a032107d655deea23993793b84852d2173
Shishwami <157940784+Shishwami@users.noreply.github.com>
2025-01-05 09:51:23 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const datasets = {
+    video: "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json",
+    kickstarter: "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json",
+    movie: "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json"
+  };
+
+  function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+  }
